44ec8421160b01b7f4cb19ff24829c8823e7444e
PULCER <anthony@pulcer.net>
2024-06-04 18:58:47 -0700

    adding validators

--- src/mealBreakCalculations.js
+++ src/mealBreakCalculations.js
@@ -1,10 +1,6 @@
 // mealBreakCalculations.js
 
 const MealBreakCalculations = {
-    // Define the bundleTimeBlocks function that takes timeBlocks as an argument and returns a bundled array three layers deep. 
-    // The innermost array should contain two elements, each representing the hour, minute, and second of the clockIn and clockOut times.
-    // The outermost array should contain all the bundled time blocks.
-  
     bundleTimeBlocks(timeBlocks) {
       return timeBlocks.map(block => [
         [parseInt(block.clockIn.hour), parseInt(block.clockIn.minute), parseInt(block.clockIn.second)],
@@ -12,9 +8,53 @@ const MealBreakCalculations = {
       ]);
     },
   
+    checkForOverlappingTimeBlocks(bundledData) {
+      for (let i = 0; i < bundledData.length; i++) {
+        const [startA, endA] = bundledData[i];
+        const startATotalSeconds = startA[0] * 3600 + startA[1] * 60 + startA[2];
+        const endATotalSeconds = endA[0] * 3600 + endA[1] * 60 + endA[2];
+  
+        for (let j = i + 1; j < bundledData.length; j++) {
+          const [startB, endB] = bundledData[j];
+          const startBTotalSeconds = startB[0] * 3600 + startB[1] * 60 + startB[2];
+          const endBTotalSeconds = endB[0] * 3600 + endB[1] * 60 + endB[2];
+  
+          if (
+            (startATotalSeconds < endBTotalSeconds && endATotalSeconds > startBTotalSeconds) ||
+            (startBTotalSeconds < endATotalSeconds && endBTotalSeconds > startATotalSeconds)
+          ) {
+            console.log('Overlapping timeblocks detected');
+            return;
+          }
+        }
+      }
+      console.log('No overlapping time blocks detected');
+    },
+  
+    validateTimeBlocks(bundledData) {
+      for (let i = 0; i < bundledData.length; i++) {
+        const [start, end] = bundledData[i];
+        const startTotalSeconds = start[0] * 3600 + start[1] * 60 + start[2];
+        const endTotalSeconds = end[0] * 3600 + end[1] * 60 + end[2];
+  
+        if (endTotalSeconds <= startTotalSeconds) {
+          console.log(`Invalid time block detected: Clock out time is not after clock in time for block ${i + 1}`);
+          return false;
+        }
+      }
+      console.log('All time blocks are valid');
+      return true;
+    },
+  
     runMealBreakCalculations(timeBlocks) {
       const bundledData = this.bundleTimeBlocks(timeBlocks);
       console.log('Bundled Data:', bundledData);
+  
+      if (!this.validateTimeBlocks(bundledData)) {
+        return;
+      }
+  
+      this.checkForOverlappingTimeBlocks(bundledData);
       // Further processing logic will go here
       return bundledData;
     }
